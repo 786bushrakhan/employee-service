b08a22ff51e6ef1530a1606d552525ab74642f90
786bushrakhan <bushrakhan250896@gmail.com>
2025-02-02 22:07:26 +0530

    added get employee api

--- src/main/java/com/example/employee_service/controller/EmployeeController.java
+++ src/main/java/com/example/employee_service/controller/EmployeeController.java
@@ -4,11 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
-    @GetMapping("/")
-    public String hello() {
-        return "Hello Spring Boot";
+public class EmployeeController {
+    @GetMapping("/api/employees")
+    public String getEmployee(){
+        return "Bushra";
+
     }
-
 }
-
